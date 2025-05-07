6bb664782e616b16f474bbd7ae8e5cf943aa09f1
Hieu Tran <tr.kimhieu@gmail.com>
2025-05-07 16:28:28 +0700

    Refactor navigation structure to remove deprecated sections and add 'Resources' and 'Insights' links

--- src/navigation.ts
+++ src/navigation.ts
@@ -54,91 +54,6 @@ export const headerData = {
         },
       ],
     },
-    // {
-    //   text: 'Pages',
-    //   links: [
-    //     {
-    //       text: 'Features (Anchor Link)',
-    //       href: getPermalink('/#features'),
-    //     },
-
-    //     {
-    //       text: 'Pricing',
-    //       href: getPermalink('/pricing'),
-    //     },
-    //     {
-    //       text: 'About us',
-    //       href: getPermalink('/about'),
-    //     },
-    //     {
-    //       text: 'Contact',
-    //       href: getPermalink('/contact'),
-    //     },
-    //     {
-    //       text: 'Terms',
-    //       href: getPermalink('/terms'),
-    //     },
-    //     {
-    //       text: 'Privacy policy',
-    //       href: getPermalink('/privacy'),
-    //     },
-    //   ],
-    // },
-    // {
-    //   text: 'Landing',
-    //   links: [
-    //     {
-    //       text: 'Lead Generation',
-    //       href: getPermalink('/landing/lead-generation'),
-    //     },
-    //     {
-    //       text: 'Long-form Sales',
-    //       href: getPermalink('/landing/sales'),
-    //     },
-    //     {
-    //       text: 'Click-Through',
-    //       href: getPermalink('/landing/click-through'),
-    //     },
-    //     {
-    //       text: 'Product Details (or Services)',
-    //       href: getPermalink('/landing/product'),
-    //     },
-    //     {
-    //       text: 'Coming Soon or Pre-Launch',
-    //       href: getPermalink('/landing/pre-launch'),
-    //     },
-    //     {
-    //       text: 'Subscription',
-    //       href: getPermalink('/landing/subscription'),
-    //     },
-    //   ],
-    // },
-    // {
-    // text: 'Blog',
-    // href: getBlogPermalink(),
-    // links: [
-    //   {
-    //     text: 'Blog List',
-    //     href: getBlogPermalink(),
-    //   },
-    //   {
-    //     text: 'Article',
-    //     href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-    //   },
-    //   {
-    //     text: 'Article (with MDX)',
-    //     href: getPermalink('markdown-elements-demo-post', 'post'),
-    //   },
-    //   {
-    //     text: 'Category Page',
-    //     href: getPermalink('tutorials', 'category'),
-    //   },
-    //   {
-    //     text: 'Tag Page',
-    //     href: getPermalink('astro', 'tag'),
-    //   },
-    // ],
-    // },
     {
       text: 'About us',
       href: getPermalink('/about'),
@@ -178,10 +93,20 @@ export const footerData = {
       ],
     },
     {
-      title: 'Legal',
+      title: 'Resources',
       links: [
-        { text: 'Privacy Policy', href: getPermalink('/privacy') },
-        { text: 'Terms of Service', href: getPermalink('/terms') },
+        {
+          text: 'Insights',
+          href: getPermalink('/insights/'),
+        },
+        {
+          text: 'Dev Hub',
+          href: getPermalink('/dev-hub/'),
+        },
+        {
+          text: 'Playbook',
+          href: getPermalink('/playbook/'),
+        },
       ],
     },
   ],
